b8042da2d09e5255dfb1a387b9f67d21c07d4013
V Accardi <aerhycke@gmail.com>
2026-01-23 22:43:05 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'game-center-v5-2';
+const CACHE_NAME = 'game-center-v5-4'; // Updated version number
 const urlsToCache = [
   './',
   './index.html',
@@ -7,19 +7,42 @@ const urlsToCache = [
   'https://unpkg.com/html5-qrcode'
 ];
 
+// Install event: Caches the files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Fetch event: Serves files from cache if available
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) return response;
+        if (response) {
+          return response;
+        }
         return fetch(event.request);
       })
+  );
+});
+
+// Activate event: Cleans up old caches (Critical for updates!)
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
