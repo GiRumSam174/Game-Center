5e8639e2e37f3f87d7c9d4cc1f4fd15afe237df7
V Accardi <aerhycke@gmail.com>
2026-01-24 23:56:35 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,24 @@
-const CACHE_NAME = 'game-center-v7.2';
-const URLS_TO_CACHE = [
+const CACHE_NAME = 'game-center-v7.3';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  // External Libraries (Cached so they work offline)
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
   'https://unpkg.com/html5-qrcode',
   'https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js',
   'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => {
+      if (k !== CACHE_NAME) return caches.delete(k);
+  }))));
 });
 
-// Clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
 });
