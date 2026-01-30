5a52a0a93854afcaf7c4601f3fb9fe2884b7b3c5
V Accardi <aerhycke@gmail.com>
2026-01-30 03:19:34 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,22 @@
-const CACHE_NAME = 'euchre-v2-1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'game-center-v8.4';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://unpkg.com/html5-qrcode',
+  'https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js',
+  'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
 ];
-
-// Install Event: Cache assets
+// ... keep the rest of the file exactly as before ...
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-
-// Activate Event: Cleanup old caches
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => {
+      if (k !== CACHE_NAME) return caches.delete(k);
+  }))));
 });
-
-// Fetch Event: Serve from cache, fall back to network
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
 });
